17b58f1f980de45788c82b4e05539bd9e9326286
Tahir Naushasd <t.naushad25@gmail.com>
2017-06-23 10:18:01 +0100

    setup

---
+++ Fiver.Mvc.ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Mvc.ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Model Validation");
+        }
+    }
+}
--- Fiver.Mvc.ModelValidation/Program.cs
+++ Fiver.Mvc.ModelValidation/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Fiver.Mvc.ModelValidation
@@ -16,7 +12,6 @@ namespace Fiver.Mvc.ModelValidation
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
